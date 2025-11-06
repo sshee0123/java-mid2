3642e0561915685a1038ce94954cffd6606c3d45
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2025-11-07 00:19:31 +0900

    제네릭메서드

---
+++ src/generic/test/ex4/MethodMain1.java
@@ -0,0 +1,20 @@
+package generic.test.ex4;
+
+public class MethodMain1 {
+
+    public static void main(String[] args) {
+        Integer i = 10;
+        Object object = GenericMethod.objMethod(i);
+
+        // 타입 인자 (Type Argument) 명시적 전달
+        System.out.println("명시적 타입 인자 전달");
+        Integer result = GenericMethod.<Integer>genericMethod(i);
+        Number integerValue = GenericMethod.<Number>numberMethod(10);
+        Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+        System.out.println("타입 추론");
+        Integer result2 = GenericMethod.genericMethod(i);
+        Integer integerValue2 = GenericMethod.numberMethod(10);
+        Double DoubleValue1 = GenericMethod.numberMethod(20.0);
+    }
+}
---
+++ src/generic/test/ex4/MethodMain3.java
@@ -0,0 +1,25 @@
+package generic.test.ex4;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain3 {
+    public static void main(String[] args) {
+        Dog dog = new Dog("멍멍이", 100);
+        Cat cat = new Cat("야옹이", 30);
+
+        ComplexBox<Dog> hospital = new ComplexBox<>();
+        hospital.set(dog);
+
+        Cat returnCat = hospital.printAndReturn(cat);
+        System.out.println("returnCat = " + returnCat);
+
+    }
+}
+
+/**
+ * animal.className: class generic.animal.Dog
+ * t.className: generic.animal.Cat
+ * returnCat = Animal{name='야옹이', size=30}
+ */
